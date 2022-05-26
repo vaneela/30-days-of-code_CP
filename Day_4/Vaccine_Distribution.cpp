699bc0a6fcde0e2ba391a3d63be23d82200e4302
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-05-26 22:00:53 +0530

    day-4

--- Day_4/Vaccine_Distribution.cpp
+++ Day_4/Vaccine_Distribution.cpp
@@ -1,28 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-void solve(){
+void solve()
+{
 
-    int n,d;
+    int n, d;
     cin >> n >> d;
     int risk = 0, nrisk = 0;
-    vector<int>v(n);
-    for(int i=0;i<n;i++){
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
         cin >> v[i];
-        if(v[i]<=9 || v[i]>=80){
+        if (v[i] <= 9 || v[i] >= 80)
+        {
             risk++;
-        }else{
+        }
+        else
+        {
             nrisk++;
         }
     }
-    int d1=ceil(float(risk)/d), d2=ceil(float(nrisk)/d);
-    cout << d1+d2 << endl;
-    
+    int d1 = ceil(float(risk) / d), d2 = ceil(float(nrisk) / d);
+    cout << d1 + d2 << endl;
 }
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         solve();
     }
     return 0;
