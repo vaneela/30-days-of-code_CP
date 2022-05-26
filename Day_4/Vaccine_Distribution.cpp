cc195fae534b6a5c36c20358e094be1f28a45b94
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-05-26 21:58:15 +0530

    day-3

---
+++ Day_4/Vaccine_Distribution.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void solve(){
+
+    int n,d;
+    cin >> n >> d;
+    int risk = 0, nrisk = 0;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+        if(v[i]<=9 || v[i]>=80){
+            risk++;
+        }else{
+            nrisk++;
+        }
+    }
+    int d1=ceil(float(risk)/d), d2=ceil(float(nrisk)/d);
+    cout << d1+d2 << endl;
+    
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
